fb1de33d22721a5ef224a53a32dd73132c7eedc4
oleh lavrik <oleh.lavrik@gmail.com>
2020-06-20 21:14:51 +0300

    Init

---
+++ src/js/modules/modals.js
@@ -0,0 +1,49 @@
+const modals = () => {
+
+    // bind modals
+    function bindModal(triggerSelector, modalSelector, closeSelector) {
+        const trigger = document.querySelectorAll(triggerSelector),
+            modal = document.querySelector(modalSelector),
+            close = document.querySelector(closeSelector);
+
+        trigger.forEach(item => {
+            item.addEventListener('click', (e) => {
+                if(e.target) {
+                    e.preventDefault();
+                }
+
+                modal.style.display = 'block';
+                document.body.style.overflow = 'hidden';
+            });
+        });
+
+        close.addEventListener('click', () => {
+            modal.style.display = 'none';
+            document.body.style.overflow = '';
+        });
+
+        modal.addEventListener('click', (e) => {
+            if(e.target === modal) {
+                modal.style.display = 'none';
+                document.body.style.overflow = '';
+            }
+        });
+    }
+
+    // init popup engineer
+    bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
+    // init popup
+    bindModal('.phone_link', '.popup', '.popup .popup_close');
+
+    function showModelByTime(selector, time) {
+        setTimeout(() => {
+            document.querySelector(selector).style.display = 'block';
+            document.body.style.overflow = 'hidden';
+        }, time);
+    }
+
+    // init show modal by time when user more than 60 sec
+    showModelByTime('.popup', 60000);
+};
+
+export default modals;
