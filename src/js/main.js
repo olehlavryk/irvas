9d06dfe547150c09b59561f13d58caf33e2218a0
oleh lavrik <oleh.lavrik@gmail.com>
2020-06-28 14:24:41 +0300

    Ajax forms added

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,11 @@
 import './slider';
 import modals from "./modules/modals";
 import tabs from "./modules/tabs";
+import forms from "./modules/form";
 
 window.addEventListener('DOMContentLoaded', () => {
+    "use strict";
+
     // popup modals
     modals();
 
@@ -10,4 +13,6 @@ window.addEventListener('DOMContentLoaded', () => {
     tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
     // tabs init for decoration
     tabs('.decoration_slider', '.decoration_tab', '.decoration_content > div > div', 'after_click');
+
+    forms();
 });
