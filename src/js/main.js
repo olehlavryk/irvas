6faf28a0a29c21c707afe97482b17cae230fc0c1
oleh lavrik <oleh.lavrik@gmail.com>
2020-07-02 09:50:16 +0300

    Add calc modal winwow step by step

--- src/js/main.js
+++ src/js/main.js
@@ -14,5 +14,8 @@ window.addEventListener('DOMContentLoaded', () => {
     // tabs init for decoration
     tabs('.decoration_slider', '.decoration_tab', '.decoration_content > div > div', 'after_click');
 
+    // window tabs in popup
+    tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
+
     forms();
 });
--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,39 +1,46 @@
 const modals = () => {
 
     // bind modals
-    function bindModal(triggerSelector, modalSelector, closeSelector) {
+    function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
         const trigger = document.querySelectorAll(triggerSelector),
             modal = document.querySelector(modalSelector),
-            close = document.querySelector(closeSelector);
+            close = document.querySelector(closeSelector),
+            windows = document.querySelectorAll('[data-modal]');
 
+        // open modal
         trigger.forEach(item => {
             item.addEventListener('click', (e) => {
                 if(e.target) {
                     e.preventDefault();
                 }
 
+                windows.forEach(item => {
+                    item.style.display = 'none';
+                });
+
                 modal.style.display = 'block';
                 document.body.style.overflow = 'hidden';
             });
         });
 
+        // close modal
         close.addEventListener('click', () => {
+            windows.forEach(item => {
+                item.style.display = 'none';
+            });
+
             modal.style.display = 'none';
             document.body.style.overflow = '';
         });
 
+        // close on modal overlay
         modal.addEventListener('click', (e) => {
-            if(e.target === modal) {
+            if(e.target === modal && closeClickOverlay) {
                 modal.style.display = 'none';
                 document.body.style.overflow = '';
             }
         });
     }
-
-    // init popup engineer
-    bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
-    // init popup
-    bindModal('.phone_link', '.popup', '.popup .popup_close');
 
     function showModelByTime(selector, time) {
         setTimeout(() => {
@@ -44,6 +51,20 @@ const modals = () => {
 
     // init show modal by time when user more than 60 sec
     //showModelByTime('.popup', 60000);
+
+    // init popup engineer
+    bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
+    // init popup
+    bindModal('.phone_link', '.popup', '.popup .popup_close');
+
+    // init popup calculate step 1
+    bindModal('.popup_calc_btn','.popup_calc', '.popup_calc_close');
+
+    // init popup calculate step 2
+    bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false)
+
+    // init popup calculate step 3
+    bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false)
 };
 
 export default modals;
